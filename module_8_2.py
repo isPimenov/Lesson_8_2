39f3e666216d4c15fb651190987bb76573821ca7
isPimenov <ipimenow@gmail.com>
2024-09-28 11:07:13 +0300

    Update module_8_2.py

--- module_8_2.py
+++ module_8_2.py
@@ -30,3 +30,4 @@ print(f'Результат 1: {calculate_average("1, 2, 3")}') # Строка п
 print(f'Результат 2: {calculate_average([1, "Строка", 3, "Ещё Строка"])}') # Учитываются только 1 и 3
 print(f'Результат 3: {calculate_average(567)}') # Передана не коллекция
 print(f'Результат 4: {calculate_average([42, 15, 36, 13])}') # Всё должно работать
+print(f'Результат 5: {calculate_average(1,2,3)}')
